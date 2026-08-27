ee498266ecae76ef89db282dcd849d3fbf08d077
milo3155-svg <milo3155@gmail.com>
2026-08-26 21:29:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // API pública para extraer info de audio sin bloqueos
-const PIPED_API = 'https://pipedapi.smnz.de';
+const PIPED_API = 'https://pipedapi.tokhmi.xyz';
 // 1. ENDPOINT DE BÚSQUEDA
 app.get('/api/search', async (req, res) => {
     const query = req.query.q;
