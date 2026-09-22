b02ec8876ec4be43baf88a5e7d11fe9a52eb18b8
milo3155-svg <milo3155@gmail.com>
2026-09-22 17:09:51 -0600

    correccion de usuarios

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def extraer_audio():
     'audioformat': 'mp3',
     'outtmpl': '%(id)s.%(ext)s',
     'quiet': True,
-    'extractor_args': {'youtube': {'player_client': ['android', 'web']}}
+    'extractor_args': {'youtube': {'player_client': ['ios', 'android_creator', 'tv']}}
 }
     
     try:
