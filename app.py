7847f5c01c41f11a5dfa9f3d5a9f1de7a51c4299
milo3155-svg <milo3155@gmail.com>
2026-09-22 13:46:36 -0600

    nuevamente guiones bajo 3

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ def extraer_audio():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
