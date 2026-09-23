436469b1c366d0cc6c89e132f340735370ce9f7d
milo3155-svg <milo3155@gmail.com>
2026-09-23 10:26:39 -0600

    de nuevo cookies

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def extraer_audio():
 'audioformat': 'mp3',
 'outtmpl': '%(id)s.%(ext)s',
 'quiet': True,
+'cookiefile':'cookies.txt',
 'extractor_args': {'youtube': {'player_client': ['tv', 'mweb']}}
 }
     
